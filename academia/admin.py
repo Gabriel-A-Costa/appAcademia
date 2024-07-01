127659f96fc2ed0eef0eb86cf38b30689a6c3c48
Gabriel Alcantara <gabrielallcantaracosta@gmail.com>
2024-07-01 16:10:12 -0300

    add treinos in admin painel

--- academia/admin.py
+++ academia/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Aparelho, Exercicio, Room
+from .models import Aparelho, Exercicio, Room, Treinos
 
 # Register your models here.
 admin.site.register(Aparelho)
 admin.site.register(Exercicio)
 admin.site.register(Room)
+admin.site.register(Treinos)
