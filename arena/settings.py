8cab0445d1a684c5eb29bb0397a2ca92e64cda95
Gabriel Alcantara <gabrielallcantaracosta@gmail.com>
2024-07-08 19:25:28 -0300

    Add cookie config

--- arena/settings.py
+++ arena/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from environ import Env
 import dj_database_url
@@ -20,10 +21,6 @@ ENVIRONMENT = env('ENVIRONMENT', default='production')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-import os
-
-
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +38,7 @@ else:
 
 ALLOWED_HOSTS = ['appacademia-production.up.railway.app', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = [ 'https://appacademia-production.up.railway.app' ]
+CSRF_TRUSTED_ORIGINS = ['https://appacademia-production.up.railway.app']
 
 
 # Application definition
@@ -58,7 +55,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'cloudinary',
     'academia.apps.AcademiaConfig',
-    'users.apps.UsersConfig', 
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-#teste de commit
+# teste de commit
 
 # Internationalization ed
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
@@ -143,10 +140,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'  
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')  
+    os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_URL = '/media/'
@@ -165,6 +162,9 @@ else:
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
 
 
 LOGIN_REDIRECT_URL = 'academia:index'
