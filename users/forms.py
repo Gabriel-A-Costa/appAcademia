2d9480ab995f7b0b189f3b9d4301cf18b288be56
Gabriel Alcantara <gabrielallcantaracosta@gmail.com>
2024-07-16 14:31:46 -0300

    configs

--- users/forms.py
+++ users/forms.py
@@ -7,7 +7,8 @@ from .models import Training, Profile, Anamnese, Antropometria
 
 
 class RememberMeForm(AuthenticationForm):
-    remember_me = forms.BooleanField(required = False, initial = True, widget=forms.CheckboxInput(attrs={'class':'login__check-input'}))
+    remember_me = forms.BooleanField(required=False, initial=True, widget=forms.CheckboxInput(
+        attrs={'class': 'login__check-input'}))
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -15,22 +16,31 @@ class RememberMeForm(AuthenticationForm):
             return username.lower()
         return username
 
+
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(label="", required=False, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Primeiro Nome'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Segundo Nome'}))
+    email = forms.EmailField(label="", required=False, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Primeiro Nome'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Segundo Nome'}))
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2',]
+        fields = ['username', 'email', 'first_name',
+                  'last_name', 'password1', 'password2',]
+
 
 class UpdateUserForm(UserChangeForm):
 
     password = None
 
-    email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
-    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Primeiro Nome'}))
-    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Segundo Nome'}))
+    email = forms.EmailField(label="", widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
+    first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Primeiro Nome'}))
+    last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Segundo Nome'}))
 
     class Meta:
         model = User
@@ -42,24 +52,28 @@ class ChangePasswordForm(SetPasswordForm):
         model = User
         fields = ['new_password1', 'new_password2']
 
+
 class TrainingPlanning(forms.ModelForm):
-    
+
     class Meta:
-        model =  Training
-        fields = ['type', 'aparelho', 'qnt_series', 'qnt_repetitions', 'tempo', 'treino']
-        
+        model = Training
+        fields = ['type', 'aparelho', 'qnt_series',
+                  'qnt_repetitions', 'tempo', 'treino', 'tipo_treino']
+
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
 
         if user and isinstance(user, User):
-            self.fields['aparelho'].queryset = Aparelho.objects.filter(users=user)
+            self.fields['aparelho'].queryset = Aparelho.objects.filter(
+                users=user)
+
 
 class FormAnamnese(forms.ModelForm):
     class Meta:
         model = Anamnese
-        fields = ['meta', 'pratica_atividades', 'atividades_fisicas', 'frequencia_atividades', 'fuma', 'tempo_fumante', 
-                  'qnt_cigarro', 'restricao', 'tipos_restricao', 'medicamento', 'tipos_medicamento', 'dores', 
-                  'tipos_dores', 'acidente', 'tipos_acidentes', 'dieta', 'alergia', 'tipos_alergia', 
+        fields = ['meta', 'pratica_atividades', 'atividades_fisicas', 'frequencia_atividades', 'fuma', 'tempo_fumante',
+                  'qnt_cigarro', 'restricao', 'tipos_restricao', 'medicamento', 'tipos_medicamento', 'dores',
+                  'tipos_dores', 'acidente', 'tipos_acidentes', 'dieta', 'alergia', 'tipos_alergia',
                   'controle_pressao', 'cardiaco', 'historico_cardiaco', 'idade', 'coluna']
 
     def __init__(self, *args, user=None, **kwargs):
@@ -67,20 +81,22 @@ class FormAnamnese(forms.ModelForm):
         if user:
             self.instance.user = user.user
 
+
 class FormAntropometria(forms.ModelForm):
     class Meta:
         model = Antropometria
         fields = [
-            'massa_corporal', 'estatura', 'pescoco', 'ombro','torax', 'cintura', 'abdome', 'quadril', 'braco_direito', 
-            'ante_braco_direito', 'coxa_direito', 'panturrilha_direito', 'braco_esquerda', 'ante_braco_esquerda', 
+            'massa_corporal', 'estatura', 'pescoco', 'ombro', 'torax', 'cintura', 'abdome', 'quadril', 'braco_direito',
+            'ante_braco_direito', 'coxa_direito', 'panturrilha_direito', 'braco_esquerda', 'ante_braco_esquerda',
             'coxa_esquerda', 'panturrilha_esquerda', 'imc', 'gordura', 'massa_corporal_2', 'metabolismo',
             'idade_corporal', 'gordura_viceral', 'avaliado'
         ]
-    
+
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
         if user:
             self.instance.user = user.user
+
 
 class ProfileImageForm(forms.ModelForm):
     class Meta:
--- users/models.py
+++ users/models.py
@@ -4,13 +4,16 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django_resized import ResizedImageField
 
-# Create your models here.  
+# Create your models here.
+
 
 class Training(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     type = models.ForeignKey(Exercicio, on_delete=models.CASCADE)
     aparelho = models.ForeignKey(Aparelho, on_delete=models.CASCADE)
-    treino = models.ForeignKey(Treinos, on_delete=models.CASCADE, null=True, blank=True)
+    treino = models.ForeignKey(
+        Treinos, on_delete=models.CASCADE, null=True, blank=True)
+    tipo_treino = models.CharField(max_length=200, null=True, blank=True)
     tempo = models.CharField(max_length=200, null=True, blank=True)
     qnt_series = models.IntegerField(null=True, blank=True)
     qnt_repetitions = models.CharField(max_length=200, null=True, blank=True)
@@ -18,7 +21,7 @@ class Training(models.Model):
 
     def __str__(self):
         return f"{self.type} - {self.aparelho.aparelho_name}"
-    
+
 
 class Anamnese(models.Model):
     META = [
@@ -51,8 +54,10 @@ class Anamnese(models.Model):
     # ANAMNESE
     meta = models.CharField(max_length=50, choices=META)
     pratica_atividades = models.CharField(max_length=5, choices=OPCOES)
-    atividades_fisicas = models.CharField(max_length=300, null=True, blank=True)
-    frequencia_atividades = models.CharField(max_length=300, null=True, blank=True)
+    atividades_fisicas = models.CharField(
+        max_length=300, null=True, blank=True)
+    frequencia_atividades = models.CharField(
+        max_length=300, null=True, blank=True)
     fuma = models.CharField(max_length=5, choices=OPCOES)
     tempo_fumante = models.CharField(max_length=200, null=True, blank=True)
     qnt_cigarro = models.IntegerField(null=True, blank=True)
@@ -60,24 +65,26 @@ class Anamnese(models.Model):
     tipos_restricao = models.CharField(max_length=200, null=True, blank=True)
     medicamento = models.CharField(max_length=5, choices=OPCOES)
     tipos_medicamento = models.CharField(max_length=200, null=True, blank=True)
-    dores = models.CharField(max_length=5, choices=OPCOES) 
+    dores = models.CharField(max_length=5, choices=OPCOES)
     tipos_dores = models.CharField(max_length=200, null=True, blank=True)
-    acidente = models.CharField(max_length=5, choices=OPCOES) 
+    acidente = models.CharField(max_length=5, choices=OPCOES)
     tipos_acidentes = models.CharField(max_length=200, null=True, blank=True)
     dieta = models.CharField(max_length=5, choices=OPCOES)
-    alergia = models.CharField(max_length=5, choices=OPCOES) 
+    alergia = models.CharField(max_length=5, choices=OPCOES)
     tipos_alergia = models.CharField(max_length=200, null=True, blank=True)
-    controle_pressao = models.CharField(max_length=10, choices=PRESSAO) 
+    controle_pressao = models.CharField(max_length=10, choices=PRESSAO)
     pressao = models.CharField(max_length=200, null=True, blank=True)
-    cardiaco = models.CharField(max_length=5, choices=OPCOES) 
-    historico_cardiaco = models.CharField(max_length=200, null=True, blank=True)
+    cardiaco = models.CharField(max_length=5, choices=OPCOES)
+    historico_cardiaco = models.CharField(
+        max_length=200, null=True, blank=True)
     idade = models.IntegerField()
-    coluna = models.CharField(max_length=200, choices=COLUNA) 
+    coluna = models.CharField(max_length=200, choices=COLUNA)
     data_anamnese = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
-    
+
+
 class Antropometria(models.Model):
 
     AVALIADO = [
@@ -88,43 +95,50 @@ class Antropometria(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     # Perimetria e Biometria (kg e cm)
-    massa_corporal = models.FloatField(null=True, blank=True)
-    estatura = models.FloatField(null=True, blank=True)
-    pescoco = models.FloatField(null=True, blank=True)
-    ombro = models.FloatField(null=True, blank=True)
-    torax = models.FloatField(null=True, blank=True)
-    cintura = models.FloatField(null=True, blank=True)
-    abdome = models.FloatField(null=True, blank=True)
-    quadril = models.FloatField(null=True, blank=True)
+    massa_corporal = models.CharField(max_length=200, null=True, blank=True)
+    estatura = models.CharField(max_length=200, null=True, blank=True)
+    pescoco = models.CharField(max_length=200, null=True, blank=True)
+    ombro = models.CharField(max_length=200, null=True, blank=True)
+    torax = models.CharField(max_length=200, null=True, blank=True)
+    cintura = models.CharField(max_length=200, null=True, blank=True)
+    abdome = models.CharField(max_length=200, null=True, blank=True)
+    quadril = models.CharField(max_length=200, null=True, blank=True)
 
-    #Perimetria direita
-    braco_direito = models.FloatField(null=True, blank=True)
-    ante_braco_direito = models.FloatField(null=True, blank=True)
-    coxa_direito = models.FloatField(null=True, blank=True)
-    panturrilha_direito = models.FloatField(null=True, blank=True)
+    # Perimetria direita
+    braco_direito = models.CharField(max_length=200, null=True, blank=True)
+    ante_braco_direito = models.CharField(
+        max_length=200, null=True, blank=True)
+    coxa_direito = models.CharField(max_length=200, null=True, blank=True)
+    panturrilha_direito = models.CharField(
+        max_length=200, null=True, blank=True)
 
-    #Perimetria esquerda
-    braco_esquerda= models.FloatField(null=True, blank=True)
-    ante_braco_esquerda= models.FloatField(null=True, blank=True)
-    coxa_esquerda= models.FloatField(null=True, blank=True)
-    panturrilha_esquerda= models.FloatField(null=True, blank=True)
+    # Perimetria esquerda
+    braco_esquerda = models.CharField(max_length=200, null=True, blank=True)
+    ante_braco_esquerda = models.CharField(
+        max_length=200, null=True, blank=True)
+    coxa_esquerda = models.CharField(max_length=200, null=True, blank=True)
+    panturrilha_esquerda = models.CharField(
+        max_length=200, null=True, blank=True)
 
-    imc = models.FloatField(null=True, blank=True)
-    gordura = models.FloatField(null=True, blank=True)
-    massa_corporal_2 = models.FloatField(null=True, blank=True)
-    metabolismo = models.FloatField(null=True, blank=True)
-    idade_corporal = models.FloatField(null=True, blank=True)
-    gordura_viceral = models.FloatField(null=True, blank=True)
+    imc = models.CharField(max_length=200, null=True, blank=True)
+    gordura = models.CharField(max_length=200, null=True, blank=True)
+    massa_corporal_2 = models.CharField(max_length=200, null=True, blank=True)
+    metabolismo = models.CharField(max_length=200, null=True, blank=True)
+    idade_corporal = models.CharField(max_length=200, null=True, blank=True)
+    gordura_viceral = models.CharField(max_length=200, null=True, blank=True)
 
-    avaliado = models.CharField(max_length=50, choices=AVALIADO, default='ativo')
+    avaliado = models.CharField(
+        max_length=50, choices=AVALIADO, default='ativo')
     data_antropometria = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_img = ResizedImageField(size=[600, 600], quality=85, upload_to='academia/users/', default= 'academia/users/default-user.jpg')
+    profile_img = ResizedImageField(
+        size=[600, 600], quality=85, upload_to='academia/users/', default='academia/users/default-user.jpg')
     phone_number = models.CharField(max_length=16, null=True)
 
     trainings = models.ManyToManyField(Training)
@@ -139,5 +153,6 @@ def creat_profile(sender, instance, created, **kwargs):
     if created and not hasattr(instance, 'profile'):
         user_profile = Profile(user=instance)
         user_profile.save()
+
 
 post_save.connect(creat_profile, sender=User)
